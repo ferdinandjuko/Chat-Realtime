936db737ae6bec68418cab29e93c362923fa7280
Ny Aina <nyainalovanirina@gmail.com>
2023-07-12 16:54:32 +0300

    decomment path setavatar

--- front/src/App.js
+++ front/src/App.js
@@ -4,7 +4,7 @@ import Register from './pages/Register'
 import Login from './pages/Login'
 import Chat from './pages/Chat'
 import TestSocket from './utils/TestSocket'
-// import SetAvatar from './pages/SetAvatar'
+import SetAvatar from './pages/SetAvatar'
 
 export default function App() {
   return (
@@ -12,7 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/setAvatar" element={<SetAvatar />} /> */}
+        <Route path="/setAvatar" element={<SetAvatar />} />
         <Route path="/" element={<Chat />} />
         {/* <Route path="/register" element={<Register />} /> */}
         <Route path="/testSocket" element={<TestSocket />} />
