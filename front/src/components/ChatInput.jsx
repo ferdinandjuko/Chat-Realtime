703a74ddccab644f5bbefb504574353b29ecfb12
rferdinandjuko <rferdinandjuko@gmail.com>
2026-05-07 18:07:05 +0300

    emoji-picker and EmojiPickerReact className

--- front/src/components/ChatInput.jsx
+++ front/src/components/ChatInput.jsx
@@ -100,8 +100,8 @@ const Container = styled.div`
     grid-template-columns: 5% 95%;
     align-items: center;
     background-color: #080420;
-    padding: 0 2rem;
-    padding-bottom: 0.3rem;
+    padding: 0 2rem 0.3rem;
+
     @media screen and (min-width: 720px) and (max-width: 1080px) {
         padding: 0 1rem;
         gap: 1rem;
@@ -111,6 +111,7 @@ const Container = styled.div`
         align-items: center;
         color: white;
         gap: 1rem;
+
         .emoji {
             position: relative;
             svg {
@@ -119,27 +120,36 @@ const Container = styled.div`
                 cursor: pointer;
                 margin: -9px;
             }
-            .EmojiPickerReact {
+            
+            .emoji-picker {
                 position: absolute;
+
+                /* Better than top: -450px */
                 bottom: 3rem;
-                background-color: #080420;
-                box-shadow: 0 5px 10px #9a86f3;
-                border-color: #9186f3;
-                .emoji-scroll-wrapper::-webkit-scrollbar-track {
+                left: 0;
+
+                z-index: 1000;
+
+                .EmojiPickerReact {
                     background-color: #080420;
-                    width:  5px;
-                    &-thumb {
-                        background-color: #9186f3;
-                    }
-                }
-                .emoji-categories {
-                    button {
-                        filter: contrast(0);
-                    }
-                }
-                .emoji-search {
-                    background-color: transparent;
+                    box-shadow: 0 5px 10px #9a86f3;
                     border-color: #9186f3;
+                    .emoji-scroll-wrapper::-webkit-scrollbar-track {
+                        background-color: #080420;
+                        width:  5px;
+                        &-thumb {
+                            background-color: #9186f3;
+                        }
+                    }
+                    .emoji-categories {
+                        button {
+                            filter: contrast(0);
+                        }
+                    }
+                    .emoji-search {
+                        background-color: transparent;
+                        border-color: #9186f3;
+                    }
                 }
             }
         }
@@ -152,6 +162,7 @@ const Container = styled.div`
         align-items: center;
         gap: 2rem;
         background-color: #ffffff34;
+
         input {
             width: 90%;
             height: 60%;
@@ -160,13 +171,16 @@ const Container = styled.div`
             border: none;
             padding-left: 1rem;
             font-size: 1.2rem;
+
             &::selection {
                 background-color: #9a86f3;
             }
+
             &:focus {
                 outline: none;
             }
         }
+
         button {
             padding: .3rem 2rem;
             border-radius: 2rem;
@@ -175,15 +189,19 @@ const Container = styled.div`
             align-items: center;
             background-color: #9a86f3;
             border: none;
-            @media screen and (min-width: 720px) and (max-width: 1080px) {
-                padding: .3rem 1rem;
-                svg {
-                    font-size: 1rem;
-                }
-            }
+            cursor: pointer;
+
             svg {
                 font-size: 2rem;
                 color: white;
+            }
+
+            @media screen and (min-width: 720px) and (max-width: 1080px) {
+                padding: .3rem 1rem;
+
+                svg {
+                    font-size: 1rem;
+                }
             }
         }
     }
