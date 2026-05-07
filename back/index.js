5053de63bde641c1b3ec191939753701e1fa94b8
rferdinandjuko <rferdinandjuko@gmail.com>
2026-05-07 16:34:05 +0300

    set listen fns for render pltfrm

--- back/index.js
+++ back/index.js
@@ -22,10 +22,12 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoute);
 
+const PORT = process.env.PORT || 5000;
+
 mongoose.connection.once('open', () => {
   console.log('Connected to MongoDB');
-  server.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`);
+  server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server started on port ${PORT}`);
   })
 })
 
